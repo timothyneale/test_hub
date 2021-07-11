14321a98cc987f80e4afd25ed054351db02df8e8
timothyneale <timothy@Timothys-MBP.modem>
2021-07-11 10:13:01 +1000

    here is the first commit

---
+++ test_hub2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 11 10:01:44 2021
+
+@author: timothy
+"""
+hello = 'hello github'
+
+
+print(hello)
+
+
+hello = 'this is an edited string'
+
+print(hello)
